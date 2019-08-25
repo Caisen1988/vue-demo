080d79f5cd4a9cf50e5315b4c9973730f3e1fa06
Nick Cai <Nick_Cai@keyoutech.com>
2019-08-25 21:11:16 +0800

    child to parent

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>i am  a cmp {{msg}}</div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg: 'hello world'
+        }
+    }
+}
+</script>
+<style>
+div{
+    background-color: red;
+}
+
+</style>
